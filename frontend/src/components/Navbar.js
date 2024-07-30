c13139e9b68b9759e1480e3f97ab1651b0bf1521
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-07-30 19:19:58 -0400

    Navbar terminado

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 import logo from '../img/logo-navbar-removebg.png';
 
 const Navbar = () => {
+  const [navbarClass, setNavbarClass] = useState('navbar transparent');
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setNavbarClass('navbar solid');
+    } else {
+      setNavbarClass('navbar transparent');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={navbarClass}>
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           <img src={logo} alt="Synapse Dev Logo" />
