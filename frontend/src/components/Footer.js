47cfb4c79cd6bda94f8a9e18ea89294e6466cffb
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-08-01 21:04:49 -0400

    Footer listo

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../css/Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faYoutube, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>© 2024 Synapse Dev. Todos los derechos reservados.</p>
+      <ul className="social-media">
+        <li><a href="#"><FontAwesomeIcon icon={faFacebookF} /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faYoutube} /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faTiktok} /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+      </ul>
     </footer>
   );
 };
