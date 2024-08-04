bb109f0373c59cc8a3c8b9b873de5b7b40d74577
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-08-03 21:00:27 -0400

    Agregar captcha

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,38 @@
-require('dotenv').config(); // Añade esto al inicio del archivo
-
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('./config/db');
+const axios = require('axios');
 
 const app = express();
-
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Importar rutas
-const projectRoutes = require('./router/projectRoutes');
-const postRoutes = require('./router/postRoutes');
+app.post('/send-email', async (req, res) => {
+  const { token, name, email, message } = req.body;
 
-app.use('/api/projects', projectRoutes);
-app.use('/api/posts', postRoutes);
+  const secretKey = process.env.RECAPTCHA_SECRET_KEY;
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${token}`;
 
-// Test de conexión a la base de datos
-db.authenticate()
-  .then(() => console.log('Database connected...'))
-  .catch(err => console.log('Error: ' + err));
+  try {
+    const response = await axios.post(verifyUrl);
+    if (!response.data.success) {
+      return res.status(400).json({ error: 'Failed captcha verification' });
+    }
+
+    // Aquí envías el email usando EmailJS o cualquier servicio de tu elección
+    emailjs.send(
+      'service_1msab3m',
+      'template_xiw5q6u',
+      { name, email, message },
+      'SlG90DUKfXFv5Dr8I'
+    )
+      .then(() => res.status(200).json({ success: 'Mensaje enviado correctamente' }))
+      .catch((error) => res.status(500).json({ error: 'Error al enviar el correo' }));
+
+  } catch (error) {
+    res.status(500).json({ error: 'Error al verificar reCAPTCHA' });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/components/ContactSection.js
+++ frontend/src/components/ContactSection.js
@@ -1,37 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import ReCAPTCHA from 'react-google-recaptcha';
 import '../css/ContactSection.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faInstagram, faYoutube, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-
 const ContactSection = () => {
+  const [recaptchaToken, setRecaptchaToken] = useState(null);
+
+  const onReCAPTCHAChange = (token) => {
+    setRecaptchaToken(token);
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!recaptchaToken) {
+      alert("Por favor, completa el reCAPTCHA.");
+      return;
+    }
+
+    const formData = new FormData(e.target);
+    formData.append('token', recaptchaToken);
+
+    fetch('/send-email', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          alert('Mensaje enviado con éxito');
+        } else {
+          alert('Hubo un error al enviar el mensaje');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('Hubo un error al enviar el mensaje');
+      });
+
+    e.target.reset();
+  };
+
   return (
     <section className="contact-section">
       <div className="contact-content">
         <div className="contact-form-wrapper">
-        <h2>Contáctanos</h2>
-        <br/>
-          <form className="contact-form">
+          <h2>Contáctanos</h2>
+          <br/>
+          <form className="contact-form" onSubmit={sendEmail}>
             <div className="form-group">
-              <input type="text" placeholder="Nombre" />
+              <input type="text" name="name" placeholder="Nombre" required />
               <div className="phone-email">
-                <input type="text" placeholder="+56 Teléfono" />
-                <input type="email" placeholder="E-mail" />
+                <input type="text" name="phone" placeholder="+56 Teléfono" />
+                <input type="email" name="email" placeholder="E-mail" required />
               </div>
-              <textarea placeholder="Mensaje"></textarea>
+              <textarea name="message" placeholder="Mensaje" required></textarea>
             </div>
+            <ReCAPTCHA
+              sitekey="6LdR7R4qAAAAAE2eXndfpsP1iiCtvEuUIJhSbwBv"
+              onChange={onReCAPTCHAChange}
+            />
             <button type="submit" className="submit-button">Enviar</button>
           </form>
           <div className="social-media">
-
             <ul>
-                <li><a href="#"><FontAwesomeIcon icon={faFacebookF} /></a></li>
-                <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
-                <li><a href="#"><FontAwesomeIcon icon={faYoutube} /></a></li>
-                <li><a href="#"><FontAwesomeIcon icon={faTiktok} /></a></li>
-                <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faFacebookF} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faYoutube} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faTiktok} /></a></li>
+              <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
             </ul>
-
           </div>
         </div>
         <div className="map-wrapper">
